b3b070d7753ae264188b9e7edc38db65b43eb025
mengshuqiang <57295330+mengshuqiang@users.noreply.github.com>
2020-12-06 23:14:17 +0800

    Create shuqiang.js

---
+++ shuqiang.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Annict Episode Checker
+// @namespace    https://github.com/Ibemu
+// @version      0.1
+// @description  個人ページや放送予定ページでマウスオーバーした作品のエピソード一覧をポップアップします
+// @author       Ibemu
+// @match        https://annict.jp/@*
+// @match        https://annict.jp/programs
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log(12324);
+})();
